6724f4828ac8e609e402cc864d37e0c41eff94be
Augusto Mosettig <augustomosettig@yahoo.com.ar>
2021-12-27 09:30:04 -0300

    conexion a product

--- src/components/Container/Producto/product.js
+++ src/components/Container/Producto/product.js
@@ -1,49 +1,53 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 import { Link, useParams } from "react-router-dom";
 
-import getProducts from '../services/handMadePromis';
+import { data } from '../services/handMadePromis';
 
 
 
 
 const Product = () => {
 
-    
- 
 
-    let {postId} = useParams();
+
+    const {postId} = useParams();
     console.log(postId)
 
 
-    const [products, setProducts] = useState()
-   
-   console.log(products)
-    //console.log('Los producots en el hook', products)
-    useEffect(() => {
-        
-        getProducts.then(res => {
-            setProducts(res)
-            
-        }).catch(err => alert('Algo no salio como se esperaba', err))
-    },[]);
 
-  
+    
 
-
-    const p = products.find(products => products.id == postId);
-
+    const p = data.find(o => o.id == postId)
     console.log(p)
 
+    
+//      const [products, setProducts] = useState()
+   
+//    console.log(products)
+//     //console.log('Los producots en el hook', products)
+//     useEffect(() => {
+        
+//         data.then(res => {
+//             setProducts(res)
+            
+//         }).catch(err => alert('Algo no salio como se esperaba', err))
+//     },[]);
+//     const p = products.find(products => products.id == postId);
+
+//     console.log(p)
+    
+
     return(
+        
         <div>
-                 <h1>Usted selecciono</h1>
                 
-                 <img src={p.image} className="imagen"></img>
+                <h1>Usted selecciono</h1>
+                <img src={p.image} className="imagen" alt = 'imagen'></img>
                 <span>{p.descripcion}</span> 
                 <h4>Precio: {p.price}</h4>
                 <h5>Cantidades diponibles: {p.stock}</h5> 
-                
+                 
             
 
 
--- src/components/Container/items.js
+++ src/components/Container/items.js
@@ -8,7 +8,7 @@ const Item = ({descripcion, price, stock, title, initials, image, id})=> {
     return(
         <div  className="borderCard" >
             <Link to = {`/producto/${id}`} >
-            <img src={image} className="imagen"></img>
+            <img src={image} className="imagen" alt = 'imagen'></img>
             <h3>{title}</h3>
             <h2>{price}</h2>
             <p>{descripcion}</p>
